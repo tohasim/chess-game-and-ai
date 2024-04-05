e95fddf80d65a52090d2156391a8a08177c4e8f0
Tohasim <toreh.simonsen@live.dk>
2024-04-05 16:47:13 +0200

    Icons added for pieces

--- script.js
+++ script.js
@@ -14,14 +14,17 @@ function init() {
 //#region VIEW
 function initBoard() {
 	const board = document.getElementById("board");
-	for (let i = 0; i < 8; i++) {
+	for (let i = 7; i >= 0; i--) {
 		for (let j = 0; j < 8; j++) {
 			const newCell = document.createElement("div");
 			newCell.classList.add("cell");
 			(i + 1 * 8 + j) % 2 === 0
 				? newCell.classList.add("black")
 				: newCell.classList.add("white");
-			newCell.textContent = `${model[i][j].color}${model[i][j].value}`;
+			if (model[i][j].icon != null) {
+				newCell.style.backgroundImage = `url(${model[i][j].icon})`;
+				newCell.style.backgroundSize = "cover";
+			}
 			board.appendChild(newCell);
 		}
 	}
@@ -30,35 +33,36 @@ function initBoard() {
 
 //#region MODEL
 class Piece {
-	constructor(color = "", value = "", row = 0, col = 0) {
+	constructor(color = "", value = "", row = 0, col = 0, icon) {
 		this.color = color;
 		this.value = value;
 		this.row = row;
 		this.col = col;
+		this.icon = icon;
 	}
 }
 
 const model = [];
 function initModel() {
 	let row = [];
-	row.push(new Piece("w", "r", 0, 0));
-	row.push(new Piece("w", "kn", 0, 1));
-	row.push(new Piece("w", "s", 0, 2));
-	row.push(new Piece("w", "k", 0, 3));
-	row.push(new Piece("w", "q", 0, 4));
-	row.push(new Piece("w", "s", 0, 5));
-	row.push(new Piece("w", "kn", 0, 6));
-	row.push(new Piece("w", "r", 0, 7));
+	row.push(new Piece("w", "r", 0, 0, "Chess_pieces/WhiteRook.png"));
+	row.push(new Piece("w", "kn", 0, 1, "Chess_pieces/WhiteKnight.png"));
+	row.push(new Piece("w", "s", 0, 2, "Chess_pieces/WhiteBishop.png"));
+	row.push(new Piece("w", "k", 0, 3, "Chess_pieces/WhiteKing.png"));
+	row.push(new Piece("w", "q", 0, 4, "Chess_pieces/WhiteQueen.png"));
+	row.push(new Piece("w", "s", 0, 5, "Chess_pieces/WhiteBishop.png"));
+	row.push(new Piece("w", "kn", 0, 6, "Chess_pieces/WhiteKnight.png"));
+	row.push(new Piece("w", "r", 0, 7, "Chess_pieces/WhiteRook.png"));
 	model.push(row);
 	row = [];
-	row.push(new Piece("w", "p", 1, 0));
-	row.push(new Piece("w", "p", 1, 1));
-	row.push(new Piece("w", "p", 1, 2));
-	row.push(new Piece("w", "p", 1, 3));
-	row.push(new Piece("w", "p", 1, 4));
-	row.push(new Piece("w", "p", 1, 5));
-	row.push(new Piece("w", "p", 1, 6));
-	row.push(new Piece("w", "p", 1, 7));
+	row.push(new Piece("w", "p", 1, 0, "Chess_pieces/WhitePawn.png"));
+	row.push(new Piece("w", "p", 1, 1, "Chess_pieces/WhitePawn.png"));
+	row.push(new Piece("w", "p", 1, 2, "Chess_pieces/WhitePawn.png"));
+	row.push(new Piece("w", "p", 1, 3, "Chess_pieces/WhitePawn.png"));
+	row.push(new Piece("w", "p", 1, 4, "Chess_pieces/WhitePawn.png"));
+	row.push(new Piece("w", "p", 1, 5, "Chess_pieces/WhitePawn.png"));
+	row.push(new Piece("w", "p", 1, 6, "Chess_pieces/WhitePawn.png"));
+	row.push(new Piece("w", "p", 1, 7, "Chess_pieces/WhitePawn.png"));
 	model.push(row);
 	for (let i = 0; i < 4; i++) {
 		row = [];
@@ -68,24 +72,24 @@ function initModel() {
 		model.push(row);
 	}
 	row = [];
-	row.push(new Piece("b", "p", 6, 0));
-	row.push(new Piece("b", "p", 6, 1));
-	row.push(new Piece("b", "p", 6, 2));
-	row.push(new Piece("b", "p", 6, 3));
-	row.push(new Piece("b", "p", 6, 4));
-	row.push(new Piece("b", "p", 6, 5));
-	row.push(new Piece("b", "p", 6, 6));
-	row.push(new Piece("b", "p", 6, 7));
+	row.push(new Piece("b", "r", 7, 0, "Chess_pieces/BlackPawn.png"));
+	row.push(new Piece("b", "kn", 7, 1, "Chess_pieces/BlackPawn.png"));
+	row.push(new Piece("b", "s", 7, 2, "Chess_pieces/BlackPawn.png"));
+	row.push(new Piece("b", "k", 7, 3, "Chess_pieces/BlackPawn.png"));
+	row.push(new Piece("b", "q", 7, 4, "Chess_pieces/BlackPawn.png"));
+	row.push(new Piece("b", "s", 7, 5, "Chess_pieces/BlackPawn.png"));
+	row.push(new Piece("b", "kn", 7, 6, "Chess_pieces/BlackPawn.png"));
+	row.push(new Piece("b", "r", 7, 7, "Chess_pieces/BlackPawn.png"));
 	model.push(row);
 	row = [];
-	row.push(new Piece("b", "r", 7, 0));
-	row.push(new Piece("b", "kn", 7, 1));
-	row.push(new Piece("b", "s", 7, 2));
-	row.push(new Piece("b", "k", 7, 3));
-	row.push(new Piece("b", "q", 7, 4));
-	row.push(new Piece("b", "s", 7, 5));
-	row.push(new Piece("b", "kn", 7, 6));
-	row.push(new Piece("b", "r", 7, 7));
+	row.push(new Piece("b", "p", 6, 0, "Chess_pieces/BlackRook.png"));
+	row.push(new Piece("b", "p", 6, 1, "Chess_pieces/BlackKnight.png"));
+	row.push(new Piece("b", "p", 6, 2, "Chess_pieces/BlackBishop.png"));
+	row.push(new Piece("b", "p", 6, 3, "Chess_pieces/BlackKing.png"));
+	row.push(new Piece("b", "p", 6, 4, "Chess_pieces/BlackQueen.png"));
+	row.push(new Piece("b", "p", 6, 5, "Chess_pieces/BlackBishop.png"));
+	row.push(new Piece("b", "p", 6, 6, "Chess_pieces/BlackKnight.png"));
+	row.push(new Piece("b", "p", 6, 7, "Chess_pieces/BlackRook.png"));
 	model.push(row);
 }
 
